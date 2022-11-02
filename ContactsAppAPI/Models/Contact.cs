d77014ee217b13b87c13be88b177cd957af2d253
Szczepan <szczep.kowalik@gmail.com>
2022-11-02 22:28:46 +0100

    Contact model

---
+++ ContactsAppAPI/Models/Contact.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ContactsAppAPI.Models
+{
+    public class Contact
+    {
+        public bool Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Phone { get; set; }
+        public string Category { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+    }
+}
