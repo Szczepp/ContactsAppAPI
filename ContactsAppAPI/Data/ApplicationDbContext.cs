31048080a503599b26dfa6805632635c39d2e1f3
Szczepan <szczep.kowalik@gmail.com>
2022-11-02 23:58:32 +0100

    ApplicationDbContext

---
+++ ContactsAppAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using ContactsAppAPI.Authentication;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactsAppAPI.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
