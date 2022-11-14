88a1da06262a013046488d3e74cef5c4a1272f16
Szczepan <szczep.kowalik@gmail.com>
2022-11-14 11:40:36 +0100

    IServiceBase, get methods async

---
+++ ContactsAppAPI/Abstracts/IServiceBase.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq.Expressions;
+using System.Linq;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+
+namespace ContactsAppAPI.Abstracts
+{
+    public interface IServiceBase<EntityType> where EntityType  : class
+    {
+        public Task<List<EntityType>> GetAllAsync();
+        public Task<EntityType> GetByIdAsync(long entityId);
+        public IEnumerable<EntityType> GetByCondition(Expression<Func<EntityType, bool>> expression);
+        public void Create(EntityType entity);
+        public void Update(EntityType entity);
+        public void Delete(long entityId);
+    }
+}
